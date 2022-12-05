76ca407cb80d3e4d000a7c986033624edad6b75e
lrwx00t <lrwx00t@gmail.com>
2022-12-04 20:25:22 -0500

    update diagram

--- src/mandelbrotlib/lib.rs
+++ src/mandelbrotlib/lib.rs
@@ -1,27 +1,27 @@
-
 use image::{self, RgbImage};
 
-const MAX_ITERATION: i32 = 300;
+const MAX_ITERATION: i32 = 30;
 
 pub fn set_rgb(iteration: i32) -> image::Rgb<u8> {
-    if iteration > MAX_ITERATION {
-        return image::Rgb([255, 255, 255]);
-    }
     let col = (((iteration as f32) / (MAX_ITERATION as f32)) * 255.0).round() as u8;
-    return image::Rgb([col, col, col]);
+    // print!("{}\n", col);
+    return image::Rgb([130, 35, col]);
 }
 
 pub fn generate_image(width: u32, height: u32) -> image::RgbImage {
     let mut img = RgbImage::new(width, height);
-    for w in 0..width - 100 {
-        let x0 = ((w as f32) / (width as f32)) * 3.5 - 2.5;
-        for h in 0..height - 100 {
+    for w in 0..width {
+        let x0 = (((w as f32) / (width as f32)) * 3.5) - 2.5;
+        for h in 0..height {
             let mut x = 0.0;
             let mut y = 0.0;
-            let y0 = ((h as f32) / (height as f32)) * 2.5 - 1.12;
+            let y0 = (((h as f32) / (height as f32)) * 2.5) - 1.2;
             let mut iteration = 0;
+            // x^2 + y^2 <= 4.0
             while x * x + y * y <= 4.0 && iteration < MAX_ITERATION {
+                // x^2 - y^2
                 let tmp = x * x - y * y + x0;
+                // 2xy
                 y = 2.0 * x * y + y0;
                 x = tmp;
                 iteration += 1;
